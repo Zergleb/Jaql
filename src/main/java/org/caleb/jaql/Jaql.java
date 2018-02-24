2fe3ae97c41e390479d5c51c2b29efb458d2781e
Caleb Holt <Boneless1213@gmail.com>
2018-02-24 00:36:54 -0700

    Update Jaql.java

--- src/main/java/org/caleb/jaql/Jaql.java
+++ src/main/java/org/caleb/jaql/Jaql.java
@@ -3,19 +3,20 @@ package org.caleb.jaql;
 import com.google.common.base.Splitter;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.validator.routines.RegexValidator;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static java.nio.charset.StandardCharsets.*;
 
 public class Jaql {
-    private String nextLine = "";
+    String nextLine = "";
+    private static final RegexValidator NAME_REGEX = new RegexValidator("-.*?-.*?name.*?: *?([a-zA-Z1-9 ]+)");
+    private static final RegexValidator SQL_COMMENT = new RegexValidator("--.*?");
 
     private Map<String, String> queriesByName = new HashMap<>();
 
@@ -24,38 +25,34 @@ public class Jaql {
         Iterable<String> lines = Splitter.on("\n").omitEmptyStrings().trimResults().split(inputString);
         Iterator<String> linesIterator = lines.iterator();
 
-        while (linesIterator.hasNext()) {
-            String line;
-            if (StringUtils.isNotBlank(nextLine)) {
+        while(linesIterator.hasNext()) {
+            String line = "";
+            if(StringUtils.isNotBlank(nextLine)) {
                 line = nextLine;
                 nextLine = "";
             } else {
                 line = linesIterator.next();
             }
-
-            String regex = "^\\s*--\\s*name:\\s*";
-            Pattern namePattern = Pattern.compile(regex);
-            Matcher nameMatcher = namePattern.matcher(line);
-            if (nameMatcher.find()) {
-                String name = line.replaceAll(regex, "");
-                StringBuilder sb = new StringBuilder();
-                boolean firstLine = true;
-                while (linesIterator.hasNext()) {
+            String[] matches = NAME_REGEX.match(line);
+            if(matches != null && matches.length == 1) {
+               String name = StringUtils.strip(matches[0]);
+               StringBuilder sb = new StringBuilder();
+               boolean firstLine = true;
+               while(linesIterator.hasNext()) {
                     String queryLine = linesIterator.next();
-                    nameMatcher = namePattern.matcher(queryLine);
-                    if (nameMatcher.find()) {
+                    if(NAME_REGEX.isValid(queryLine)) {
                         nextLine = queryLine;
                         break;
                     }
-
-                    if (!firstLine) {
-                        sb.append("\n");
+                    if(SQL_COMMENT.isValid(queryLine) == false) {
+                        if(firstLine == false) {
+                            sb.append("\n");
+                            firstLine = false;
+                        }
+                        sb.append(queryLine);
                     }
-
-                    sb.append(queryLine);
-                    firstLine = false;
-                }
-                queriesByName.put(name, sb.toString());
+               }
+               queriesByName.put(name, sb.toString());
             }
         }
     }
