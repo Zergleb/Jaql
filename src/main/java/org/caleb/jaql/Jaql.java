9b7959095e23c243f76a57faee490067ad457ad4
Caleb Holt <caleb.holt@collectivemedicaltech.com>
2019-10-31 00:48:16 -0600

    Added variable replacement. still no query param escaping so unsafe

--- src/main/java/org/caleb/jaql/Jaql.java
+++ src/main/java/org/caleb/jaql/Jaql.java
@@ -8,6 +8,8 @@ import org.apache.commons.validator.routines.RegexValidator;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import static java.nio.charset.StandardCharsets.*;
 
@@ -17,47 +19,59 @@ public class Jaql {
     private static final RegexValidator SQL_COMMENT = new RegexValidator("--.*");
     private static final String MULTILINE_COMMENT = "(?s)\\/\\*.*?\\*\\/";
     private static final String INLINE_COMMENT = "\\s*--.*";
+    private final String replaceTagsStart;
+    private final String replaceTagsEnd;
 
     private Map<String, String> queriesByName = new HashMap<>();
 
-    public Jaql(InputStream resourceAsStream) throws IOException {
+    public Jaql(InputStream resourceAsStream, String replaceTagsStart, String replaceTagsEnd) throws IOException {
         String inputString = IOUtils.toString(resourceAsStream, UTF_8);
         inputString = inputString.replaceAll(MULTILINE_COMMENT, " ");
 
         Iterable<String> lines = Splitter.on("\n").omitEmptyStrings().trimResults().split(inputString);
         Iterator<String> linesIterator = lines.iterator();
 
-        while(linesIterator.hasNext()) {
+        while (linesIterator.hasNext()) {
             String line = "";
-            if(StringUtils.isNotBlank(nextLine)) {
+            if (StringUtils.isNotBlank(nextLine)) {
                 line = nextLine;
                 nextLine = "";
             } else {
                 line = linesIterator.next();
             }
             String[] matches = NAME_REGEX.match(line);
-            if(matches != null && matches.length == 1) {
-               String name = StringUtils.strip(matches[0]);
-               StringBuilder sb = new StringBuilder();
-               boolean firstLine = true;
-               while(linesIterator.hasNext()) {
+            if (matches != null && matches.length == 1) {
+                String name = StringUtils.strip(matches[0]);
+                StringBuilder sb = new StringBuilder();
+                boolean firstLine = true;
+                while (linesIterator.hasNext()) {
                     String queryLine = linesIterator.next();
-                    if(NAME_REGEX.isValid(queryLine)) {
+                    if (NAME_REGEX.isValid(queryLine)) {
                         nextLine = queryLine;
                         break;
                     }
-                    if(SQL_COMMENT.isValid(queryLine) == false) {
-                        if(firstLine == false) {
+                    if (SQL_COMMENT.isValid(queryLine) == false) {
+                        if (firstLine == false) {
                             sb.append("\n");
                         }
                         queryLine = queryLine.replaceAll(INLINE_COMMENT, "");
                         sb.append(queryLine);
                         firstLine = false;
                     }
-               }
-               queriesByName.put(name, sb.toString());
+                }
+                queriesByName.put(name, sb.toString());
             }
         }
+        this.replaceTagsStart = replaceTagsStart;
+        this.replaceTagsEnd = replaceTagsEnd;
+    }
+
+    public String getQueryByName(String name, Map<String, String> replaceValues) {
+        String query = queriesByName.get(name);
+        for(Entry<String, String> entry : replaceValues.entrySet()) {
+            query = query.replace(replaceTagsStart + entry.getKey() + replaceTagsEnd, entry.getValue());
+        }
+        return query;
     }
 
     public String getQueryByName(String name) {
--- src/test/java/org/caleb/jaql/JaqlTest.java
+++ src/test/java/org/caleb/jaql/JaqlTest.java
@@ -1,15 +1,19 @@
 package org.caleb.jaql;
 
 import static org.junit.Assert.*;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 public class JaqlTest {
 
     @Test
-    public void testNothing() throws Throwable {
-        Jaql jaql = new Jaql(JaqlTest.class.getResourceAsStream("testFile.sql"));
+    public void testRetrieveTemplates() throws Throwable {
+        Jaql jaql = new Jaql(JaqlTest.class.getResourceAsStream("testFile.sql"), "${", "}");
 
-        assertEquals(12, jaql.getQueryNames().size());
+        assertEquals(14, jaql.getQueryNames().size());
         assertNull(jaql.getQueryByName("doesntExist"));
         assertNull(jaql.getQueryByName("badNameLocation"));
         assertEquals("select * from something;", jaql.getQueryByName("firstQuery"));
@@ -27,5 +31,12 @@ public class JaqlTest {
         assertNull(jaql.getQueryByName(""));
         assertEquals("select * from spacedname", jaql.getQueryByName("spaced Name Query"));
         assertEquals("select * from endquery", jaql.getQueryByName("endQuery"));
+        assertEquals("select * from templateQuery where name = 'nameValue'", jaql.getQueryByName("templateQuery", ImmutableMap.of("name", "nameValue")));
+        String hl7Result = "MSH|^~\\&|EPIC|EPICADT|SMS|SMSADT|199912271408|CHARRIS|ADT^A04|1817457|D|2.5|\n" + 
+        "PID||0493575^^^2^ID 1|454721||DOE^JOHN^^^^|DOE^JOHN^^^^|19480203|M||B|254 MYSTREET AVE^^MyTown^OH^44123^USA||(216)123-4567|||M|NON|400003403~1129086|\n" + 
+        "NK1||MyLastName^MyFirstName^^^^|SPO||(216)123-4567||EC|||||||||||||||||||||||||||\n" + 
+        "PV1||O|168 ~219~C~PMA^^^^^^^^^||||277^ALLEN MYLASTNAME^BONNIE^^^^|||||||||| ||2688684|||||||||||||||||||||||||199912271408||||||002376853";
+        String hl7ExtractedValue = jaql.getQueryByName("notAQueryButHl7ExtraUseCaseMultiline", ImmutableMap.of("LastName", "MyLastName", "FirstName", "MyFirstName", "Town", "MyTown", "ADTTYPE", "A04"));
+        assertEquals(hl7Result, hl7ExtractedValue);
     }
 }
